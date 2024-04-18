ead9e29074f66ee2d7a538d86906c41e9e5a25f5
Ahelsel <helseltony@gmail.com>
2024-04-18 03:14:30 -0600

    Moved GUI rendering to GuiRenderer.Java

--- core/src/main/java/org/ooad/project/graphics/GuiRenderer.java
+++ core/src/main/java/org/ooad/project/graphics/GuiRenderer.java
@@ -1,5 +1,44 @@
 package org.ooad.project.graphics;
 
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import org.ooad.project.level.Level;
+
 public class GuiRenderer {
-    // TODO: Implement guiRenderer
+    private BitmapFont font;
+    private SpriteBatch batch;
+    private ShapeRenderer shapeRenderer;
+    private Level level;
+
+    public GuiRenderer(Level level) {
+        font = new BitmapFont();
+        batch = new SpriteBatch();
+        this.level = level;
+        shapeRenderer = new ShapeRenderer();
+    }
+
+    public void renderEnemiesRemaining() {
+        batch.begin();
+        font.draw(batch, "Enemies remaining: " + level.getNumEnemies(), 10, level.getHeight() * 50 - 10);
+        batch.end();
+    }
+
+    public void renderGameOver(float alpha) {
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(0, 0, 0, alpha);
+        shapeRenderer.rect(0, 0, level.getWidth() * 50, level.getHeight() * 50);
+        shapeRenderer.end();
+
+        batch.begin();
+        font.setColor(1, 1, 1, alpha);
+        font.draw(batch, "Game Over", level.getWidth() * 25 - 50, level.getHeight() * 25);
+        batch.end();
+    }
+
+    public void dispose() {
+        font.dispose();
+        batch.dispose();
+    }
+
 }
--- core/src/main/java/org/ooad/project/views/GameScreen.java
+++ core/src/main/java/org/ooad/project/views/GameScreen.java
@@ -10,6 +10,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
 import org.ooad.project.Game;
+import org.ooad.project.graphics.GuiRenderer;
 import org.ooad.project.level.Level;
 import org.ooad.project.graphics.LevelRenderer;
 import org.ooad.project.graphics.TowerRenderer;
@@ -21,21 +22,21 @@ public class GameScreen implements Screen {
     private LevelRenderer levelRenderer;
     private TowerRenderer towerRenderer;
     private EnemyRenderer enemyRenderer;
+    private GuiRenderer guiRenderer;
+    private Boolean isPaused;
     private Level level;
-    private BitmapFont font;
-    private SpriteBatch batch;
-    private ShapeRenderer shapeRenderer;
     private boolean gameOver;
     private float gameOverTime;
     private float animationTime = 0f;
 
     private final Game game;
-    private Boolean isPaused;
-
 
     public GameScreen(Game game) {
         this.game = game;
         isPaused = false;
+
+        gameOver = false;
+        gameOverTime = 0;
 
         gameWidth = 9;
         gameHeight = 9;
@@ -45,14 +46,7 @@ public class GameScreen implements Screen {
         levelRenderer = new LevelRenderer(level);
         towerRenderer = new TowerRenderer(level);
         enemyRenderer = new EnemyRenderer(level);
-
-        font = new BitmapFont();
-        batch = new SpriteBatch();
-
-        gameOver = false;
-        gameOverTime = 0;
-
-        shapeRenderer = new ShapeRenderer();
+        guiRenderer   = new GuiRenderer(level);
 
         level.placeTower(level.getTile(2, (level.getHeight()/2) + 1));
         level.placeTower(level.getTile(6, (level.getHeight()/2) - 2));
@@ -68,11 +62,7 @@ public class GameScreen implements Screen {
             towerRenderer.render();
             enemyRenderer.render(animationTime);
             // @TODO: render projectiles
-            // @TODO: render GUI/health bar
-
-            batch.begin();
-            font.draw(batch, "Enemies remaining: " + level.getNumEnemies(), 10, level.getHeight() * 50 - 10);
-            batch.end();
+            guiRenderer.renderEnemiesRemaining();
 
             if (level.getNumEnemies() == 0 && level.getEnemies().isEmpty() && !gameOver) {
                 gameOver = true;
@@ -81,23 +71,14 @@ public class GameScreen implements Screen {
 
             if (gameOver) {
                 float alpha = Math.min(1, (animationTime - gameOverTime) / 4);
-
-                shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-                shapeRenderer.setColor(0, 0, 0, alpha);
-                shapeRenderer.rect(0, 0, level.getWidth() * 50, level.getHeight() * 50);
-                shapeRenderer.end();
-
-                batch.begin();
-                font.setColor(1, 1, 1, alpha);
-                font.draw(batch, "Game Over", level.getWidth() * 25 - 50, level.getHeight() * 25);
-                batch.end();
-
                 if (animationTime - gameOverTime > 3) {
-                    // Gdx.app.exit();
                     game.setScreen(new StartScreen(game));
                     dispose();
+                } else {
+                    guiRenderer.renderGameOver(alpha);
                 }
             }
+
         }
     }
 
